e84913c1c46fff32e7ebf99769b83413ffa545a5
Abdullah944 <darkside944@gmail.com>
2024-09-05 22:49:15 -0700

    change name of screen within the file

--- src/app/(tabs)/menu/[productId].tsx
+++ src/app/(tabs)/menu/[productId].tsx
@@ -1,11 +1,15 @@
 import { View, Text } from "react-native";
 import React from "react";
-import { useLocalSearchParams } from "expo-router";
+import { Stack, useLocalSearchParams } from "expo-router";
 
 const ProductDetailsScreen = () => {
   const { productId } = useLocalSearchParams(); //take form the params or the url or the routing +name of what in {} should match the one in the file naming
   return (
     <View>
+      {/* You can use all options of screen in a file also without calling a name */}
+      <Stack.Screen
+        options={{ title: "ProductDetailsScreen" + " " + productId }}
+      />
       <Text>ProductDetailsScreen</Text>
       <Text>{productId}</Text>
     </View>
