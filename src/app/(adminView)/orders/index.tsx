8fd2f5dafe0e72701ef340f646aa985e49be8d70
Abdullah944 <darkside944@gmail.com>
2024-09-11 17:21:30 -0700

    real time data + useInsertOrderSubscription hook

--- src/app/(adminView)/orders/index.tsx
+++ src/app/(adminView)/orders/index.tsx
@@ -1,10 +1,13 @@
 import { useAdminOrderList } from "@/api/orders";
+import { useInsertOrderSubscription } from "@/api/orders/subscriptions";
 import OrderListItem from "@/components/Lists/OrderListItem";
 import { Stack } from "expo-router";
 import { ActivityIndicator, FlatList, Text } from "react-native";
 
 const OrdersScreen = () => {
   const { data, error, isLoading } = useAdminOrderList({ archived: false });
+
+  useInsertOrderSubscription();
 
   if (isLoading) {
     return <ActivityIndicator />;
